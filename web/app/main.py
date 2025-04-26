a86732920877d33c055880dac63352b58346911a
Javier Mauricio Llanos Atoy <javierllanosatoy@gmail.com>
2025-04-26 15:11:18 -0500

    actualización

--- web/app/main.py
+++ web/app/main.py
@@ -1,20 +1,18 @@
-from flask import Flask, render_template, request
-import requests
-
 app = Flask(__name__)
 
-API_URL = "http://api:8000" 
+API_URL = "http://api:8000"
 
 @app.route("/")
 def home():
-    publicaciones = requests.get(f"{API_URL}/posts").json()  
+    publicaciones = requests.get(f"{API_URL}/posts").json() 
     return render_template("index.html", publicaciones=publicaciones)
 
 @app.route("/publicacion/<int:id>")
 def publicacion(id):
     pub = requests.get(f"{API_URL}/posts").json()  
-    comentarios = requests.get(f"{API_URL}/comments?post_id={id}").json() 
+    comentarios = requests.get(f"{API_URL}/comments", params={"post_id": id}).json()  
     publicacion = next((p for p in pub if p["id"] == id), None)
     return render_template("publicacion.html", publicacion=publicacion, comentarios=comentarios)
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
