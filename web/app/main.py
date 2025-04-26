dfdcb461fda8e27fc12eb4d4c2ae8f7759045449
Javier Mauricio Llanos Atoy <javierllanosatoy@gmail.com>
2025-04-26 15:03:20 -0500

    actualización

--- web/app/main.py
+++ web/app/main.py
@@ -3,17 +3,18 @@ import requests
 
 app = Flask(__name__)
 
-API_URL = "http://api:8000"  # Asegúrate de que sea el nombre del servicio en Docker
+API_URL = "http://api:8000" 
 
 @app.route("/")
 def home():
-    publicaciones = requests.get(f"{API_URL}/publicaciones").json()
+    publicaciones = requests.get(f"{API_URL}/posts").json()  
     return render_template("index.html", publicaciones=publicaciones)
 
 @app.route("/publicacion/<int:id>")
 def publicacion(id):
-    pub = requests.get(f"{API_URL}/publicaciones").json()
-    comentarios = requests.get(f"{API_URL}/comentarios/{id}").json()
+    pub = requests.get(f"{API_URL}/posts").json()  
+    comentarios = requests.get(f"{API_URL}/comments?post_id={id}").json() 
     publicacion = next((p for p in pub if p["id"] == id), None)
     return render_template("publicacion.html", publicacion=publicacion, comentarios=comentarios)
-
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
